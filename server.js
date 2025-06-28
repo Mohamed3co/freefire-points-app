d76c13c455969890da50a16534ed645d44a17157
Mohamed3co <medinekhouloud@gmail.com>
2025-06-28 17:37:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const withdrawRoute = require('./routes/withdraw');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,10 +10,8 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('🚀 السيرفر يعمل!');
-  });
+});
 
-  app.use('/withdraw', withdrawRoute);
-
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
